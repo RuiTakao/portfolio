d71b9b4b6b28a23b5662948eda2d2210a51e5822
RuiTakao <ruia1082halfnc@gmail.com>
2022-08-21 16:17:45 +0900

    first commit

---
+++ cover.js
@@ -0,0 +1,35 @@
+$(function(){
+    /* cover作る **/
+    const cover = $('<div></div>', {
+      "class": "cover"
+    })
+    const copy = $('<div></div>', {
+      "class": "copy_cover"
+    })
+    const copyImage = $('<img />', {
+      src: "img/copy.png",
+      "class": "copy_cover_img"
+    })
+
+
+
+    /* coverをbodyに追加 **/
+    $('#cover').append(cover)
+    cover.append(copy)
+    copy.append(copyImage)
+
+    copyImage.fadeIn(1500)
+    copyImage.css({
+      "transform": "scale(1)"
+    })
+    setTimeout(function() {
+      copyImage.fadeOut(1000)
+    }, 2000)
+
+    setTimeout(function() {
+      cover.stop().animate({opacity: '0'}, 1000)
+      $('.first_view').removeClass('firstTimeHiddenElement')  
+      $('.bg').removeClass('firstTimeHiddenElement')  
+      $('.footer').removeClass('firstTimeHiddenElement')  
+    }, 3000)
+});
